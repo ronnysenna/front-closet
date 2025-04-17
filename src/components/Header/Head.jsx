88ddd105784ee780d1790a100c176fb8751160f0
Marcelo <devfullmarcelo@gmail.com>
2025-04-17 17:00:47 -0300

    iitt

--- src/components/Header/Head.jsx
+++ src/components/Header/Head.jsx
@@ -7,34 +7,28 @@ export const Head = () => {
       <section className="head">
         <div className="container d_flex">
           <div className="left-row">
-            <a
-              target="_blank"
-              href="https://wa.me/5585991908723?text=Ol%C3%A1%2C%20vi%20seu%20site%20e%20tenho%20interesse"
-              className="icon-flex phone-icon"
-            >
-              <i className="fa fa-phone"></i>
-              <label className="phone-icon" htmlFor="">
-                (85) 99190-8723
-              </label>
-            </a>
-            <a
-              target="_blank"
-              href="mailto:devfullmarcelo@gmail.com"
-              className="icon-flex phone-icon"
-            >
-              <i className="fa fa-envelope"></i>
-              <label className="phone-icon" htmlFor="">
-                devfullmarcelo@gmail.com
-              </label>
-            </a>
+            <h1 className="brand-name">GoFashion</h1>
           </div>
           <div className="right-row">
             <span>
-              <label htmlFor="">Rede social</label>
+              <label htmlFor="">Siga-nos:</label>
             </span>
             <span>
-              <a href="#">
+              <a
+                target="_blank"
+                href="https://www.instagram.com"
+                className="icon-flex"
+              >
                 <i className="fa fa-instagram"></i>
+              </a>
+            </span>
+            <span>
+              <a
+                target="_blank"
+                href="https://wa.me/5585991908723"
+                className="icon-flex"
+              >
+                <i className="fa fa-whatsapp"></i>
               </a>
             </span>
           </div>
