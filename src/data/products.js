00fcb797f6593ae2003c21ce0d1d6a936dc06163
Marcelo <devfullmarcelo@gmail.com>
2025-06-07 02:32:10 -0300

    verMais

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,7 @@ export const products = [
     "id": "1748810288102",
     "title": "Camisola Suede",
     "description": "Camisola feminina feita em tecido suede, macio e confortável, ideal para noites aconchegantes.",
-    "imageUrl": "image/camisolasuede.jpeg",
+    "imageUrl": "/image/camisolasuede.jpeg",
     "price": "15.00",
     "stock": 20,
     "sizes": [
@@ -19,7 +19,7 @@ export const products = [
     "id": "1748810372782",
     "title": "Camisola Suede Plus ",
     "description": "Versão plus size da camisola em suede, garantindo conforto e elegância para todos os corpos.",
-    "imageUrl": "image/babydollplus.jpeg",
+    "imageUrl": "/image/babydollplus.jpeg",
     "price": "15.00",
     "stock": 20,
     "sizes": [
@@ -33,7 +33,7 @@ export const products = [
     "id": "1748810479345",
     "title": "Babydoll Virginia Full Baby ",
     "description": "Conjunto de babydoll delicado, com design juvenil e confortável, perfeito para quem gosta de um estilo mais romântico.",
-    "imageUrl": "image/babydollvirginia.jpeg",
+    "imageUrl": "/image/babydollvirginia.jpeg",
     "price": "15.00",
     "stock": 20,
     "sizes": [
@@ -47,7 +47,7 @@ export const products = [
     "id": "1748810595908",
     "title": "Camisola Lisa ",
     "description": "Camisola feminina de modelo simples e sem estampas, ideal para quem prefere um visual minimalista e elegante.",
-    "imageUrl": "image/babydollliso.jpeg",
+    "imageUrl": "/image/babydollliso.jpeg",
     "price": "14.00",
     "stock": 20,
     "sizes": [
@@ -71,7 +71,7 @@ export const products = [
     "id": "1748810662056",
     "title": "Camisola Tule",
     "description": "Peça sensual confeccionada em tule transparente, ideal para ocasiões especiais.",
-    "imageUrl": "image/camisolatule.jpeg",
+    "imageUrl": "/image/camisolatule.jpeg",
     "price": "18.00",
     "stock": 20,
     "sizes": [
@@ -90,7 +90,7 @@ export const products = [
     "id": "1748810723147",
     "title": "Camisola Valmir ",
     "description": "Modelo específico de camisola, que pode ter detalhes exclusivos como rendas ou cortes diferenciados, dependendo da coleção.",
-    "imageUrl": "image/camisolavalmir.jpeg",
+    "imageUrl": "/image/camisolavalmir.jpeg",
     "price": "14.00",
     "stock": 20,
     "sizes": [
@@ -106,7 +106,7 @@ export const products = [
     "id": "1748810788028",
     "title": "Baby Luxo",
     "description": "Conjunto de babydoll sofisticado, geralmente com detalhes em renda ou cetim, ideal para momentos especiais.",
-    "imageUrl": "image/babyluxo.jpeg",
+    "imageUrl": "/image/babyluxo.jpeg",
     "price": "12.00",
     "stock": 20,
     "sizes": [
@@ -123,7 +123,7 @@ export const products = [
     "id": "1748810906807",
     "title": "Babydoll Plus",
     "description": "Babydoll em modelagem plus size, que une conforto e sensualidade para mulheres de todos os tamanhos.",
-    "imageUrl": "image/babydollplus.jpeg",
+    "imageUrl": "/image/babydollplus.jpeg",
     "price": "18.00",
     "stock": 20,
     "sizes": [
@@ -144,7 +144,7 @@ export const products = [
     "id": "1748810989952",
     "title": "Babydoll Liso",
     "description": "Babydoll básico e sem estampas, focado na simplicidade e no conforto.",
-    "imageUrl": "image/babydollliso.jpeg",
+    "imageUrl": "/image/babydollliso.jpeg",
     "price": "14.00",
     "stock": 20,
     "sizes": [
@@ -165,7 +165,7 @@ export const products = [
     "id": "1748811043386",
     "title": "Mini Babydoll Luxo",
     "description": "Versão mais curta do babydoll luxo, com design sexy e elegante.",
-    "imageUrl": "image/miniluxo.jpeg",
+    "imageUrl": "/image/miniluxo.jpeg",
     "price": "12.00",
     "stock": 20,
     "sizes": [
@@ -182,7 +182,7 @@ export const products = [
     "id": "1748811099983",
     "title": "Kit Mozão",
     "description": "Conjunto de peças para casal, combinando roupas íntimas femininas e masculinas, ideal para presentear.",
-    "imageUrl": "image/kitmozao.jpeg",
+    "imageUrl": "/image/kitmozao.jpeg",
     "price": "32.00",
     "stock": 20,
     "sizes": [
@@ -196,7 +196,7 @@ export const products = [
     "id": "1748811153131",
     "title": "Kit Mãe e Filha ",
     "description": "Conjunto de roupas combinando modelos iguais ou semelhantes para mãe e filha, geralmente em estilo confortável e divertido.",
-    "imageUrl": "image/kitmaefilha.jpeg",
+    "imageUrl": "/image/kitmaefilha.jpeg",
     "price": "32.00",
     "stock": 20,
     "sizes": [
@@ -210,7 +210,7 @@ export const products = [
     "id": "1748811193781",
     "title": "Babydoll Lara",
     "description": "Modelo específico de babydoll, que pode trazer características únicas de design ou tecido, dependendo da marca ou coleção.",
-    "imageUrl": "image/babydolllara.jpeg",
+    "imageUrl": "/image/babydolllara.jpeg",
     "price": "12.00",
     "stock": 20,
     "sizes": [
@@ -224,7 +224,7 @@ export const products = [
     "id": "1748811320292",
     "title": "Baby Virgínia Full",
     "description": "Conjunto infantil delicado e confortável, com design fofo e alegre, ideal para o dia a dia ou para dormir com muito estilo.",
-    "imageUrl": "image/babydollvirginia.jpeg",
+    "imageUrl": "/image/babydollvirginia.jpeg",
     "price": "15.00",
     "stock": 20,
     "sizes": [
@@ -238,7 +238,7 @@ export const products = [
     "id": "1748811361958",
     "title": "Babydoll Lara Infantil ",
     "description": "Modelo infantil do babydoll Lara, com corte confortável e tecido leve, perfeito para proporcionar liberdade de movimento e charme para as meninas.",
-    "imageUrl": "image/infantilbabydoll.jpeg",
+    "imageUrl": "/image/infantilbabydoll.jpeg",
     "price": "12.00",
     "stock": 20,
     "sizes": [
