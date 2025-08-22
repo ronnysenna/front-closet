4f505843697f4674e7de80f0577bb1565c431fbe
Marcelo <devfullmarcelo@gmail.com>
2025-08-22 09:47:18 -0300

    voltou

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // CartProvider já está no main.jsx envolvendo o App
-import CatalogoPage from './pages/CatalogoPage';
-
+import Header from './components/Header';
+import FeedbackPage from './pages/FeedbackPage';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CategoryPage from './pages/CategoryPage';
 import Box from '@mui/material/Box'; // Para ajudar no layout do footer
 
 function App() {
   return (
     <Router>
       <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
         <Box component="main" sx={{ flexGrow: 1 }}> {/* Conteúdo principal cresce */}
           <Routes>
-            <Route path="/catalogo" element={<CatalogoPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="/categoria/:category" element={<CategoryPage />} />
+            <Route path="/feedback" element={<FeedbackPage />} />
           </Routes>
         </Box>
+        <Footer />
       </Box>
     </Router>
   );
